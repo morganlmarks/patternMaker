01f5b8444f02217eb6a76c4e3be2713824953071
morganlmarks <morganlmarks@gmail.com>
2014-09-14 02:30:46 -0700

    new cluster method is pixel location conscious

--- patternMaker.java
+++ patternMaker.java
@@ -9,6 +9,29 @@ import java.util.*;
 
 public class patternMaker {
 
+	public class ColorPair implements Comparable<ColorPair>{
+		public int colorLesser;
+		public int colorGreater;
+		public float similarity;
+		
+		public ColorPair(int color1, int color2) {
+			colorLesser = color1 > color2 ? color2 : color1;
+			colorGreater = colorLesser == color2 ? color1 : color2;
+			similarity = sim2(colorLesser, colorGreater);
+		}
+		
+		public int compareTo(ColorPair compairPair) {
+			if (compairPair.similarity < this.similarity) {
+				return -1;
+			} else if (compairPair.similarity > this.similarity) {
+				return 1;
+			} else {
+				return 0;
+			}
+		}
+	}
+	
+
 	private BufferedImage origImg;
 	private int numStrings;
 	private int imgWidth;
@@ -83,7 +106,7 @@ public class patternMaker {
 				
 				Integer rgbAvg = new Integer(rgbavg);
 				int count = 0;
-				if (!colorMap.containsKey(rgbAvg)) {
+				if (!colorCount.containsKey(rgbAvg)) {
 					Set<Integer> tempSet = new HashSet<Integer>();
 					tempSet.add(rgbAvg);
 					colorMap.put(rgbAvg, tempSet);
@@ -130,7 +153,7 @@ public class patternMaker {
 	}
 
 	public void clusterColors(int numColorsClustered)
-	{
+	{	
 		int numColors = colorCount.size();
 		while (numColors > numColorsClustered)
 		{
@@ -182,7 +205,126 @@ public class patternMaker {
 		}
 	}
 	
+	public void clusterColors2(int numColorsClustered, int numCandidates)
+	{
+		int numColors = colorCount.size();
+		HashMap<Integer, ArrayList<ColorPair>> cpTable = new HashMap<Integer, ArrayList<ColorPair>>();
+		PriorityQueue<ColorPair> cpHeap = new PriorityQueue<ColorPair>(numColors*numColors);
+		Set<Integer> startColors = colorCount.keySet();
+		Iterator<Integer> iterColors1 = startColors.iterator();
+		while (iterColors1.hasNext())
+		{
+			int color1 = iterColors1.next();
+			Iterator<Integer> iterColors2 = startColors.iterator();
+			while (iterColors2.hasNext())
+			{
+				int color2 = iterColors2.next();
+				if (color1 < color2)
+				{
+					ColorPair cp = new ColorPair(color1, color2);
+					ArrayList<ColorPair> list1 = cpTable.containsKey(color1) ? cpTable.get(color1) : new ArrayList<ColorPair>(numColors);
+					ArrayList<ColorPair> list2 = cpTable.containsKey(color2) ? cpTable.get(color2) : new ArrayList<ColorPair>(numColors);
+					list1.add(cp);
+					list2.add(cp);
+					cpTable.put(color1, list1);
+					cpTable.put(color2, list2);
+					cpHeap.add(cp);
+				}
+			}
+		}
+		while (numColors > numColorsClustered)
+		{
+			//int NUM_CANDIDATES = numColors/(4* numColorsClustered) + 2;
+			int NUM_CANDIDATES = numCandidates;
+			ArrayList<ColorPair> mergeCandidates = new ArrayList<ColorPair>(NUM_CANDIDATES);
+			float bestRatio = 100;
+			int bestIndex = 0;
+			for (int i=0; i < NUM_CANDIDATES; i++)
+			{
+				ColorPair candidate = cpHeap.poll();
+				int numStrings1 = calculateNumStrings(candidate.colorLesser);
+				int numStrings2 = calculateNumStrings(candidate.colorGreater);
+				int numStringsMerged = calculateNumStrings(candidate);
+				float stringRatio = (float)numStringsMerged/(numStrings1+numStrings2);
+				if (stringRatio < bestRatio)
+				{
+					bestRatio=stringRatio;
+					bestIndex=i;
+				}
+				mergeCandidates.add(candidate);
+			}
+			ColorPair cpToMerge = mergeCandidates.remove(bestIndex);
+			for (int i=0; i < NUM_CANDIDATES - 1; i++)
+			{
+				cpHeap.add(mergeCandidates.get(i));
+			}
+			int count1 = colorCount.get(cpToMerge.colorLesser);
+			int count2 = colorCount.get(cpToMerge.colorGreater);
+			int newColor = averageColors(cpToMerge.colorLesser, cpToMerge.colorGreater, count1, count2);
+			boolean alreadyExisted = colorCount.containsKey(newColor);
+			int maybeCount = alreadyExisted ? colorCount.get(newColor) : 0;
+			colorCount.remove(cpToMerge.colorLesser);
+			colorCount.remove(cpToMerge.colorGreater);
+			ArrayList<ColorPair> lesserList = cpTable.get(cpToMerge.colorLesser);
+			ArrayList<ColorPair> greaterList = cpTable.get(cpToMerge.colorGreater);
+			
 
+			if (cpToMerge.colorLesser != newColor)
+			{
+				for (int i=0; i < lesserList.size();i++)
+				{
+					ColorPair toRemove = lesserList.get(i);
+					cpHeap.remove(toRemove);
+				}
+				cpTable.remove(cpToMerge.colorLesser);
+			}
+			if (cpToMerge.colorGreater != newColor)
+			{
+				for (int i=0; i< greaterList.size();i++)
+				{
+					ColorPair toRemove = greaterList.get(i);
+					cpHeap.remove(toRemove);
+				}
+				cpTable.remove(cpToMerge.colorGreater);
+			}
+			
+			colorCount.put(newColor, count1 + count2 + maybeCount);
+			numColors = colorCount.size();			
+			Set<Integer> curColors = colorCount.keySet();
+			Iterator<Integer> iterColors = curColors.iterator();
+			if (!alreadyExisted)
+			{
+				ArrayList<ColorPair> newColorPairs = new ArrayList<ColorPair>(numColors);
+				while (iterColors.hasNext())
+				{
+					int color = iterColors.next();
+					if (color != newColor)
+					{
+						ColorPair cp = new ColorPair(color, newColor);
+						ArrayList<ColorPair> oldList = cpTable.get(color);
+						oldList.add(cp);
+						cpTable.put(color, oldList);
+						newColorPairs.add(cp);
+						cpHeap.add(cp);
+					}
+				}
+				cpTable.put(newColor, newColorPairs);
+			}
+			for (int x=0; x < gridWidth; x++)
+			{
+				for (int y=0; y < gridHeight; y++)
+				{
+					if (colorGrid[y][x] == cpToMerge.colorLesser || colorGrid[y][x] == cpToMerge.colorGreater)
+					{
+						colorGrid[y][x] = newColor;
+					}
+				}
+			}	
+			//this.writeImage(numColors + "colors");
+		}
+	}
+	
+	
 	public void updateColorGrid()
 	{
 		HashMap<Integer, Integer> colorFun = new HashMap<Integer, Integer>();
@@ -211,60 +353,14 @@ public class patternMaker {
 		}
 	}
 	
-	public void calculateNumStrings()
-	{
+	public int getTotalNumStrings() {
 		int sum = 0;
 		Set<Integer> curColors = colorCount.keySet();
 		Iterator<Integer> iterColors = curColors.iterator();
 		while (iterColors.hasNext())
 		{
 			int color = iterColors.next();
-			int[][] curColorGrid = new int[gridHeight*2][gridWidth];
-			for (int i=0; i< gridHeight*2;i++)
-			{
-				Arrays.fill(curColorGrid[i], -1);
-			}
-			for (int x=0; x < gridWidth; x++)
-			{
-				for (int y=0; y < gridHeight; y++)
-				{
-					if (colorGrid[y][x] == color)
-					{
-						curColorGrid[y*2 + (x%2)][x] = 0;
-					}
-				}
-			}		
-			int highest = 0;
-			for (int x=1; x < gridWidth; x++)
-			{
-				for (int y=gridHeight*2 - 1; y >= 0; y--)
-				{
-					if (curColorGrid[y][x] != -1)
-					{
-						int i = -1;
-						boolean placeFound;
-						do {
-							placeFound = true;
-							i++;
-							for (int j = Math.max(0,y - x + i); j < Math.min(gridHeight*2 - 1, y + x - i); j++)
-							{
-								if (curColorGrid[j][i] != -1)
-								{
-									if ((Math.abs(y-j)+1)/(Math.abs(x-curColorGrid[j][i])+1) == 0)
-									{
-										placeFound = false;
-									}
-								}
-							}
-						} while (!placeFound);
-						curColorGrid[y][i] = x;
-						curColorGrid[y][x] = -1;
-						highest = Math.max(highest, i);
-					}
-				}
-			}
-			System.out.println(String.format("\tColor %d requires %d strings", color, highest+1));
-			sum = sum + highest + 1;
+			sum = sum + calculateNumStrings(color);
 			/*
 			StringBuffer printArray = new StringBuffer(gridHeight*gridWidth*2);
 			for (int i=0; i< gridHeight*2; i++)
@@ -281,6 +377,105 @@ public class patternMaker {
 			*/
 		}
 		System.out.println(String.format("%d strings total are needed", sum));
+		return sum;
+	}
+	
+	public int calculateNumStrings(int color)
+	{
+		int[][] curColorGrid = new int[gridHeight*2][gridWidth];
+		for (int i=0; i< gridHeight*2;i++)
+		{
+			Arrays.fill(curColorGrid[i], -1);
+		}
+		for (int x=0; x < gridWidth; x++)
+		{
+			for (int y=0; y < gridHeight; y++)
+			{
+				if (colorGrid[y][x] == color)
+				{
+					curColorGrid[y*2 + (x%2)][x] = 0;
+				}
+			}
+		}		
+		int highest = 0;
+		for (int x=1; x < gridWidth; x++)
+		{
+			for (int y=gridHeight*2 - 1; y >= 0; y--)
+			{
+				if (curColorGrid[y][x] != -1)
+				{
+					int i = -1;
+					boolean placeFound;
+					do {
+						placeFound = true;
+						i++;
+						for (int j = Math.max(0,y - x + i); j < Math.min(gridHeight*2 - 1, y + x - i); j++)
+						{
+							if (curColorGrid[j][i] != -1)
+							{
+								if ((Math.abs(y-j)+1)/(Math.abs(x-curColorGrid[j][i])+1) == 0)
+								{
+									placeFound = false;
+								}
+							}
+						}
+					} while (!placeFound);
+					curColorGrid[y][i] = x;
+					curColorGrid[y][x] = -1;
+					highest = Math.max(highest, i);
+				}
+			}
+		}
+		return highest + 1;
+	}
+	
+	public int calculateNumStrings(ColorPair cp)
+	{
+		int[][] curColorGrid = new int[gridHeight*2][gridWidth];
+		for (int i=0; i< gridHeight*2;i++)
+		{
+			Arrays.fill(curColorGrid[i], -1);
+		}
+		for (int x=0; x < gridWidth; x++)
+		{
+			for (int y=0; y < gridHeight; y++)
+			{
+				if (colorGrid[y][x] == cp.colorLesser || colorGrid[y][x] == cp.colorGreater)
+				{
+					curColorGrid[y*2 + (x%2)][x] = 0;
+				}
+			}
+		}		
+		int highest = 0;
+		for (int x=1; x < gridWidth; x++)
+		{
+			for (int y=gridHeight*2 - 1; y >= 0; y--)
+			{
+				if (curColorGrid[y][x] != -1)
+				{
+					int i = -1;
+					boolean placeFound;
+					do {
+						placeFound = true;
+						i++;
+						for (int j = Math.max(0,y - x + i); j < Math.min(gridHeight*2 - 1, y + x - i); j++)
+						{
+							if (curColorGrid[j][i] != -1)
+							{
+								if ((Math.abs(y-j)+1)/(Math.abs(x-curColorGrid[j][i])+1) == 0)
+								{
+									placeFound = false;
+								}
+							}
+						}
+					} while (!placeFound);
+					curColorGrid[y][i] = x;
+					curColorGrid[y][x] = -1;
+					highest = Math.max(highest, i);
+				}
+			}
+		}
+		return highest + 1;
 	}
 	
 		private void loadColors()
@@ -376,10 +571,10 @@ public class patternMaker {
 		float r2 = (rgb2 >> 16) & 0xFF;
 		float g2 = (rgb2 >> 8) & 0xFF;
 		float b2 = rgb2 & 0xFF;
-		float mag1 = (float)Math.sqrt(r1*r1 + b1*b1 + g1*g1);
-		float mag2 = (float)Math.sqrt(r2*r2 + b2*b2 + g2*g2);
-		float cosSim = ((r1*r2 + b1*b2 + g1*g2)/(mag1*mag2));
-		return cosSim * cosSim * ((float)Math.min(mag1, mag2)/Math.max(mag1, mag2));
+		float mag1Squared = r1*r1 + b1*b1 + g1*g1;
+		float mag2Squared = r2*r2 + b2*b2 + g2*g2;
+		float cosSimSquared = ((r1*r2 + b1*b2 + g1*g2)*(r1*r2 + b1*b2 + g1*g2)/(mag1Squared*mag2Squared));
+		return cosSimSquared * ((float)Math.sqrt(Math.min(mag1Squared, mag2Squared)/Math.max(mag1Squared, mag2Squared)));
 	}	
 	
 	public static void main(String[] args)
@@ -396,11 +591,17 @@ public class patternMaker {
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println(String.format("Number of colors set to %d.", num2));
 		}
+		int num3 = 7;
+		try  {
+			num2 = Integer.parseInt(args[1]);
+		} catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println(String.format("Number of candidates set to %d.", num3));
+		}
 		
 		BufferedImage img = null;
 		try
 		{
-			File file = new File("C:\\Users\\Morgan\\Desktop\\face.jpg");
+			File file = new File("C:\\Users\\Morgan\\Desktop\\pic.jpg");
 			img = (BufferedImage)ImageIO.read(file);
 		} catch (Exception e)
 		{
@@ -409,13 +610,13 @@ public class patternMaker {
 		patternMaker pM = new patternMaker(img, num);
 		pM.pixelate();
 		System.out.println("Pixelating DONE");
-		pM.writeImage("C:\\Users\\Morgan\\Desktop\\sphere" + num);
-		pM.clusterColors(num2);
+		pM.writeImage("C:\\Users\\Morgan\\Desktop\\pix" + num);
+		pM.clusterColors2(num2, num3);
 		System.out.println("Clustering DONE");
-		pM.updateColorGrid();
-		pM.writeImage(String.format("C:\\Users\\Morgan\\Desktop\\sphere%d_%d", num, num2));
+		//pM.updateColorGrid();
+		int totalStrings = pM.getTotalNumStrings();
+		pM.writeImage(String.format("C:\\Users\\Morgan\\Desktop\\clustered%d_%d_%d_%d", num, num2, num3, totalStrings));
 		//System.out.println("DONE");
-		pM.calculateNumStrings();
 
 
 	}
